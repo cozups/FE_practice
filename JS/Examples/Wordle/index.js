1d19162f7cb50b4c0d99f3ed07b60e762109ea0c
Kim Miso <kimms9725@naver.com>
2022-05-28 14:49:59 +0900

    기본적 기능 구현. 수정 필요

--- JS/Examples/Wordle/index.js
+++ JS/Examples/Wordle/index.js
@@ -1,13 +1,100 @@
-var answer = "abcde";
-document.querySelector("button").addEventListener("click", function () {
-  var input = document.querySelectorAll(".input");
+// variables
+let answer = [];
+let currentRow = 0;
+let userAnswer = [];
+
+// dom element
+const table = document.querySelector('tbody');
+const submitButton = document.querySelector('button');
+const userInputList = document.querySelectorAll('input');
+// add events
+submitButton.addEventListener('click', writeTable);
+
+function tableRendering() {
+  const tableRow = `
+    <tr>
+      <td align='center'></td>
+      <td align='center'></td>
+      <td align='center'></td>
+      <td align='center'></td>
+      <td align='center'></td>
+    </tr>
+  `;
+
   for (let i = 0; i < 5; i++) {
-    if (input[i].value == answer[i]) {
-      input[i].style.background = "green";
-    } else if (answer.includes(input[i].value)) {
-      input[i].style.background = "yellow";
+    table.innerHTML += tableRow;
+  }
+}
+
+function writeTable(e) {
+  e.preventDefault();
+
+  if (currentRow >= 5) {
+    return;
+  }
+
+  const row = table.querySelectorAll('tr')[currentRow];
+  let tdList = row.querySelectorAll('td');
+
+  for (let i = 0; i < 5; i++) {
+    userAnswer.push(userInputList[i].value);
+  }
+
+  for (let i = 0; i < 5; i++) {
+    tdList[i].innerText = userAnswer[i];
+  }
+  const isAnswer = checkAnswer(tdList);
+  for (let i = 0; i < 5; i++) {
+    userInputList[i].value = '';
+  }
+  if (isAnswer) {
+    setTimeout(function () {
+      alert('정답입니다!');
+    }, 300);
+  }
+  userAnswer = [];
+  currentRow++;
+}
+
+function setAnswer() {
+  answer = ['ㅅ', 'ㅏ', 'ㄹ', 'ㅏ', 'ㅇ'];
+}
+
+function checkAnswer(tdList) {
+  let isAnswer = true;
+  for (let i = 0; i < 5; i++) {
+    const inputChar = userInputList[i].value;
+    let idx = answer.indexOf(inputChar);
+    console.log(answer[i], inputChar, idx);
+    if (idx >= 0) {
+      // 글자가 있음
+      if (idx === i || answer[i] === inputChar) {
+        // 올바른 위치
+        tdList[i].style.backgroundColor = 'green';
+      } else {
+        // 다른 위치
+        tdList[i].style.backgroundColor = 'orange';
+        isAnswer = false;
+      }
     } else {
-      input[i].style.background = "lightgrey";
+      // 글자가 없음
+      tdList[i].style.backgroundColor = 'red';
+      isAnswer = false;
     }
+  }
+  return isAnswer;
+}
+
+// execution
+tableRendering();
+setAnswer();
+
+// 글자 하나 들어오면 다음 input으로 넘기기
+$('input').on('keyup', function () {
+  const charLimit = $(this).prop('maxlength');
+
+  if ($(this).val().length >= charLimit) {
+    $(this).next('input').focus();
+    return false;
   }
 });
