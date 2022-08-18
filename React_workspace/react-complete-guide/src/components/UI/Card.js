e6f47af598a12802ef54b346ca7470d4cac8d0ed
Kim Miso <kimms9725@naver.com>
2022-08-18 13:57:48 +0900

    react-complete-guide 재설정

--- React_workspace/react-complete-guide/src/components/UI/Card.js
+++
@@ -1,9 +0,0 @@
-import "./Card.css";
-
-// reusable wrapper component
-function Card(props) {
-  const classes = "card " + props.className;
-  return <div className={classes}>{props.children}</div>; // children: 사이에 있는 콘텐츠를 사용할 수 있게 해줌.
-}
-
-export default Card;
