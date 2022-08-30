68ce1a781193c076b41cecfe27b54585b1922a22
Kim Miso <kimms9725@naver.com>
2022-08-30 18:11:49 +0900

    AddUser: state 추가 & validation

--- react-udemy/05-project-for-practice/src/Components/Users/AddUser.js
+++ react-udemy/05-project-for-practice/src/Components/Users/AddUser.js
@@ -1,8 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
+
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
+  const addUserHandler = (e) => {
+    e.preventDefault();
+
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+    if (+enteredAge < 1) {
+      return;
+    }
+    console.log(enteredUsername, enteredAge);
+    setEnteredUsername('');
+    setEnteredAge('');
+  };
+
+  const usernameChangeHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  };
+
+  const ageChangeHandler = (e) => {
+    setEnteredAge(e.target.value);
+  };
+
+  return (
+    <CardInput>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+        />
+        <label htmlFor="age">Age (Years)</label>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHandler}
+        />
+        <Button type="submit">Add User</Button>
+      </form>
+    </CardInput>
+  );
+};
+
+export default AddUser;
 
 const CardInput = styled(Card)`
   margin: 2rem auto;
@@ -30,23 +81,3 @@ const CardInput = styled(Card)`
     border-color: #4f005f;
   }
 `;
-
-const AddUser = (props) => {
-  const addUserHandler = (e) => {
-    e.preventDefault();
-  };
-
-  return (
-    <CardInput>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
-        <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
-        <Button type="submit">Add User</Button>
-      </form>
-    </CardInput>
-  );
-};
-
-export default AddUser;
