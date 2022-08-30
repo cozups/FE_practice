ff99da96b9dc4b9047efd67691d43f40d061b472
Kim Miso <kimms9725@naver.com>
2022-08-30 18:33:01 +0900

    Userlist 렌더링 및 관리

--- react-udemy/05-project-for-practice/src/App.js
+++ react-udemy/05-project-for-practice/src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import AddUser from './Components/Users/AddUser';
+import UsersList from './Components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUsersListHandler = (username, age) => {
+    setUsersList((prevUserList) => [
+      ...prevUserList,
+      {
+        id: `user${Math.random()}`,
+        name: username,
+        age: age,
+      },
+    ]);
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUsersListHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
