6cebd74d21585d3d9f14762635ed63c3e70d89ee
Kim Miso <kimms9725@naver.com>
2023-03-13 12:36:18 +0900

    useLoaderData hook

--- react-udemy/18-adv-starting-project/frontend/src/pages/Events.js
+++ react-udemy/18-adv-starting-project/frontend/src/pages/Events.js
@@ -1,23 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { useLoaderData } from 'react-router-dom';
 
-const DUMMY_EVENTS = [
-  { id: 'e1', title: 'event 1' },
-  { id: 'e2', title: 'event 2' },
-  { id: 'e3', title: 'event 3' },
-];
+import EventsList from '../components/EventsList';
 
 function EventsPage() {
+  const events = useLoaderData();
   return (
     <>
-      <h1>EventsPage</h1>
-      <ul>
-        {DUMMY_EVENTS.map((evt) => (
-          <li key={evt.id}>
-            <Link to={`/events/${evt.id}`}>{evt.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <EventsList events={events} />
     </>
   );
 }
