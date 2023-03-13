592dd146e32b2a4060f04285c4818af1d42027d9
Kim Miso <kimms9725@naver.com>
2023-03-13 13:19:17 +0900

    custom error 처리

--- react-udemy/18-adv-starting-project/frontend/src/pages/Events.js
+++ react-udemy/18-adv-starting-project/frontend/src/pages/Events.js
@@ -4,6 +4,11 @@ import EventsList from '../components/EventsList';
 
 function EventsPage() {
   const data = useLoaderData();
+
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+
   const events = data.events;
 
   return (
@@ -19,7 +24,10 @@ export async function loader() {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
-    // ...
+    // return { isError: true, message: 'Could not fetch events.' };
+    throw new Response(JSON.stringify({ message: 'Could not fetch events.' }), {
+      status: 500,
+    });
   } else {
     const resData = await response.json();
     return resData;
