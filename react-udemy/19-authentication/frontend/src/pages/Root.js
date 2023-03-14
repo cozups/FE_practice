6355a4869d52fa35466e4f2671e5f9363314aef3
Kim Miso <kimms9725@naver.com>
2023-03-14 21:19:55 +0900

    token expiration

--- react-udemy/19-authentication/frontend/src/pages/Root.js
+++ react-udemy/19-authentication/frontend/src/pages/Root.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { Outlet, useLoaderData, useSubmit } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import { getTokenDuration } from '../util/auth';
 
 function RootLayout() {
   // const navigation = useNavigation();
@@ -12,9 +13,17 @@ function RootLayout() {
       return;
     }
 
+    if (token === 'EXPIRED') {
+      submit(null, { action: '/logout', method: 'POST' });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+    console.log(tokenDuration);
+
     setTimeout(() => {
       submit(null, { action: '/logout', method: 'POST' });
-    }, 1 * 60 * 60 * 1000);
+    }, tokenDuration);
   }, [token, submit]);
 
   return (
