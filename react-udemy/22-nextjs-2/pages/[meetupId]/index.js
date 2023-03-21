e6590e818052300136db323c44255df9fde1b9c1
Kim Miso <kimms9725@naver.com>
2023-03-21 22:28:24 +0900

    detail page 구성

---
+++ react-udemy/22-nextjs-2/pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://images.unsplash.com/photo-1678737169727-a3236885e763?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=423&q=80"
+      title="First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a first meetup."
+    />
+  );
+}
+
+export default MeetupDetails;
