c5c6b441726ab8acff52633023ae1f8a365400ca
Kim Miso <kimms9725@naver.com>
2023-03-21 23:18:11 +0900

    getStaticPaths

--- react-udemy/22-nextjs-2/pages/[meetupId]/index.js
+++ react-udemy/22-nextjs-2/pages/[meetupId]/index.js
@@ -11,4 +11,40 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://images.unsplash.com/photo-1678737169727-a3236885e763?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=423&q=80',
+        id: meetupId,
+        title: 'First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup.',
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
