f3ae81272a77cfb273e078655878b79ad067e70b
Kim Miso <kimms9725@naver.com>
2023-03-23 15:39:19 +0900

    add meta data

--- react-udemy/22-nextjs-2/pages/[meetupId]/index.js
+++ react-udemy/22-nextjs-2/pages/[meetupId]/index.js
@@ -1,15 +1,22 @@
+import Head from 'next/head';
 import { MongoClient, ObjectId } from 'mongodb';
 
 import MeetupDetail from '../../components/meetups/MeetupDetail';
 
 function MeetupDetails({ meetupData }) {
   return (
-    <MeetupDetail
-      image={meetupData.image}
-      title={meetupData.title}
-      address={meetupData.address}
-      description={meetupData.description}
-    />
+    <>
+      <Head>
+        <title>{meetupData.title}</title>
+        <meta name="description" content={meetupData.description} />
+      </Head>
+      <MeetupDetail
+        image={meetupData.image}
+        title={meetupData.title}
+        address={meetupData.address}
+        description={meetupData.description}
+      />
+    </>
   );
 }
 
