a8ade0ea35f74a57892b0fa6b8a2505086d311ca
Kim Miso <kimms9725@naver.com>
2023-03-27 17:52:17 +0900

    split component

--- react-udemy/24-react-test/src/components/Greeting.js
+++ react-udemy/24-react-test/src/components/Greeting.js
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import Output from './Output';
+
 const Greeting = () => {
   const [changedText, setChangedText] = useState(false);
 
@@ -10,7 +12,11 @@ const Greeting = () => {
   return (
     <div>
       <h2>Hello World!</h2>
-      {!changedText ? <p>It's good to see you!</p> : <p>Changed!</p>}
+      {!changedText ? (
+        <Output>It's good to see you!</Output>
+      ) : (
+        <Output>Changed!</Output>
+      )}
       <button onClick={changeTextHandler}>Change Text!</button>
     </div>
   );
