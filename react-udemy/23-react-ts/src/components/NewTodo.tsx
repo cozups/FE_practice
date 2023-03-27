6d65882ebf32cde15d5e16e11933032cff7cbb1b
Kim Miso <kimms9725@naver.com>
2023-03-27 13:50:40 +0900

    create NewTodo form

---
+++ react-udemy/23-react-ts/src/components/NewTodo.tsx
@@ -0,0 +1,28 @@
+import { useRef } from 'react';
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an error
+      return;
+    }
+
+    props.onAddTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo text</label>
+      <input ref={todoTextInputRef} type="text" id="text" />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
