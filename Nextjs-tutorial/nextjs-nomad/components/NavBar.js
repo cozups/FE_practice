ca6c668375e492a81fe459ab701e0409deae6ecf
Kim Miso <kimms9725@naver.com>
2023-03-29 16:32:39 +0900

    style navbar with style jsx

--- Nextjs-tutorial/nextjs-nomad/components/NavBar.js
+++ Nextjs-tutorial/nextjs-nomad/components/NavBar.js
@@ -1,27 +1,24 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import styles from './NavBar.module.css';
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link
-        href="/"
-        className={`${styles.link} ${
-          router.pathname === '/' ? styles.active : ''
-        }`}
-      >
-        Home
+      <Link legacyBehavior href="/">
+        <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
       </Link>
-      <Link
-        href="/about"
-        className={`${styles.link} ${
-          router.pathname === '/about' ? styles.active : ''
-        }`}
-      >
-        About
+      <Link legacyBehavior href="/about">
+        <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
       </Link>
+      <style jsx>{`
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: tomato;
+        }
+      `}</style>
     </nav>
   );
 }
