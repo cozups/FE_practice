61baee4475f9b677cf6ea5c04ebc6f7057f9ae4a
Kim Miso <kimms9725@naver.com>
2023-04-19 15:56:31 +0900

    feat: fetch users from server

--- redux/redux-essentials-example-app-master/src/features/users/usersSlice.js
+++ redux/redux-essentials-example-app-master/src/features/users/usersSlice.js
@@ -1,15 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { client } from '../../api/client';
 
-const initialState = [
-  { id: '0', name: 'Tianna Jenkins' },
-  { id: '1', name: 'Kevin Grant' },
-  { id: '2', name: 'Madison Price' },
-];
+const initialState = [];
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  const response = await client.get('fakeApi/users');
+  return response.data;
+});
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      // If we return a new value, that will replace the existing state completely with whatever we return.
+      return action.payload;
+    });
+  },
 });
 
 export default usersSlice.reducer;
