b42352222ef9d46f1645f529eac9232312f2b9d4
Kim Miso <kimms9725@naver.com>
2023-04-19 14:12:11 +0900

    feat: add reaction buttons in post

--- redux/redux-essentials-example-app-master/src/features/posts/postsSlice.js
+++ redux/redux-essentials-example-app-master/src/features/posts/postsSlice.js
@@ -7,12 +7,14 @@ const initialState = [
     title: 'First Post!',
     content: 'Hello!',
     date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
   },
   {
     id: '2',
     title: 'Second Post',
     content: 'More Text',
     date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
   },
 ];
 
@@ -44,9 +46,16 @@ const postsSlice = createSlice({
         existingPost.content = content;
       }
     },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.find((post) => post.id === postId);
+      if (existingPost) {
+        existingPost.reactions[reaction]++;
+      }
+    },
   },
 });
 
-export const { postAdded, postUpdated } = postsSlice.actions;
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions;
 
 export default postsSlice.reducer;
