e7f542a3e86be264ebbd9f3774d7de28e31f78cc
Kim Miso <kimms9725@naver.com>
2023-07-19 18:00:44 +0900

    intersection observer - infinite scroll

--- JS/intersection observer/script.js
+++ JS/intersection observer/script.js
@@ -11,8 +11,32 @@ const observer = new IntersectionObserver(
   },
   {
     threshold: 1,
+    rootMargin: '100px',
   }
 );
+
+const lastCardObserver = new IntersectionObserver((entries) => {
+  const lastCard = entries[0];
+  if (!lastCard.isIntersecting) {
+    return;
+  }
+  loadNewCards();
+  lastCardObserver.unobserve(lastCard.target);
+  lastCardObserver.observe(document.querySelector('.card:last-child'));
+}, {});
+
+lastCardObserver.observe(document.querySelector('.card:last-child'));
+const cardContainer = document.querySelector('.card-container');
+
+function loadNewCards() {
+  for (let i = 0; i < 10; i++) {
+    const card = document.createElement('div');
+    card.textContent = 'New Card';
+    card.classList.add('card');
+    observer.observe(card);
+    cardContainer.append(card);
+  }
+}
 
 // 각 엘리먼트에 intersection observer 등록
 cards.forEach((card) => {
