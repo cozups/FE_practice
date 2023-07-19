d732f6355919736a004d28506130adc585706ef4
Kim Miso <kimms9725@naver.com>
2023-07-19 17:49:22 +0900

    intersection observer options

--- JS/intersection observer/script.js
+++ JS/intersection observer/script.js
@@ -2,12 +2,17 @@
 const cards = document.querySelectorAll('.card');
 
 // observer 생성
-const observer = new IntersectionObserver((entries) => {
-  // 교차 영역에 있을 때 'show' 클래스 토글
-  entries.forEach((entry) => {
-    entry.target.classList.toggle('show', entry.isIntersecting);
-  });
-});
+const observer = new IntersectionObserver(
+  (entries) => {
+    // 교차 영역에 있을 때 'show' 클래스 토글
+    entries.forEach((entry) => {
+      entry.target.classList.toggle('show', entry.isIntersecting);
+    });
+  },
+  {
+    threshold: 1,
+  }
+);
 
 // 각 엘리먼트에 intersection observer 등록
 cards.forEach((card) => {
