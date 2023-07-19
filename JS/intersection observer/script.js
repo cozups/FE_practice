950938500bc7d520259117dfc3b52262ed941868
Kim Miso <kimms9725@naver.com>
2023-07-19 17:30:01 +0900

    intersection observer 사용하기

---
+++ JS/intersection observer/script.js
@@ -0,0 +1,15 @@
+// target element
+const cards = document.querySelectorAll('.card');
+
+// observer 생성
+const observer = new IntersectionObserver((entries) => {
+  // 교차 영역에 있을 때 'show' 클래스 토글
+  entries.forEach((entry) => {
+    entry.target.classList.toggle('show', entry.isIntersecting);
+  });
+});
+
+// 각 엘리먼트에 intersection observer 등록
+cards.forEach((card) => {
+  observer.observe(card);
+});
