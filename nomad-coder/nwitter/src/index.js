c7136b8a3e98c85c397f5ab116d4f1a39590f202
Kim Miso <kimms9725@naver.com>
2023-09-05 15:54:43 +0900

    feat: read nweets

--- nomad-coder/nwitter/src/index.js
+++ nomad-coder/nwitter/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import { HashRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
-    <React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
       <App />
-    </React.StrictMode>
-  </HashRouter>
+    </HashRouter>
+  </React.StrictMode>
 );
--- nomad-coder/nwitter/src/routes/Home.js
+++ nomad-coder/nwitter/src/routes/Home.js
@@ -1,9 +1,25 @@
-import { useState } from 'react';
-import { collection, addDoc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { collection, addDoc, getDocs } from 'firebase/firestore';
 import { dbService } from 'firebaseInstance';
 
 const Home = () => {
   const [nweet, setNweet] = useState('');
+  const [nweets, setNweets] = useState([]);
+
+  const getNweets = async () => {
+    const dbNweets = await getDocs(collection(dbService, 'nweets'));
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getNweets();
+  }, []);
+
   const onSubmit = async (event) => {
     event.preventDefault();
     try {
@@ -25,6 +41,8 @@ const Home = () => {
     setNweet(value);
   };
 
+  console.log(nweets);
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -37,6 +55,14 @@ const Home = () => {
         />
         <input type="submit" value="Nweet" />
       </form>
+
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
