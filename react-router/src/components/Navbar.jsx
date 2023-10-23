48280b2ba7017dc864dec104c6ab4daea3667210
Kim Miso <kimms9725@naver.com>
2023-10-23 14:57:03 +0900

    tutorial: nested route

--- react-router/src/components/Navbar.jsx
+++ react-router/src/components/Navbar.jsx
@@ -15,6 +15,9 @@ export const Navbar = () => {
       <NavLink to="/about" style={navLinkStyles}>
         About
       </NavLink>
+      <NavLink to="/products" style={navLinkStyles}>
+        Products
+      </NavLink>
     </nav>
   );
 };
