b0c77d095a7527a0d02c4c52a827b244460709e1
Kim Miso <kimms9725@naver.com>
2023-10-24 16:11:54 +0900

    useContext

---
+++ react-hooks/src/components/ComponentF.jsx
@@ -0,0 +1,40 @@
+import { useContext } from 'react';
+import { UserContext, ChannelContext } from '../App';
+
+function ComponentF() {
+  const user = useContext(UserContext);
+  const channel = useContext(ChannelContext);
+
+  return (
+    <div>
+      User context value {user}, Channel context value {channel}
+    </div>
+  );
+}
+
+// Consumer 버전
+/*
+function ComponentF() {
+  return (
+    <div>
+      <UserContext.Consumer>
+        {(user) => {
+          return (
+            <ChannelContext.Consumer>
+              {(channel) => {
+                return (
+                  <div>
+                    User context value {user}, Channel context value {channel}
+                  </div>
+                );
+              }}
+            </ChannelContext.Consumer>
+          );
+        }}
+      </UserContext.Consumer>
+    </div>
+  );
+}
+*/
+
+export default ComponentF;
