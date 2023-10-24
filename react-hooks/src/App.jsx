3920e11ea8088bdf1a40637070306e0dde11d713
Kim Miso <kimms9725@naver.com>
2023-10-24 16:48:41 +0900

    useReducer

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,17 +1,13 @@
-import React from 'react';
-import ComponentC from './components/ComponentC';
-
-export const UserContext = React.createContext();
-export const ChannelContext = React.createContext();
+import CounterOne from './components/CounterOne';
+import CounterThree from './components/CounterThree';
+import CounterTwo from './components/CounterTwo';
 
 function App() {
   return (
     <div className="App">
-      <UserContext.Provider value={'miso'}>
-        <ChannelContext.Provider value={'cozups'}>
-          <ComponentC />
-        </ChannelContext.Provider>
-      </UserContext.Provider>
+      {/* <CounterOne /> */}
+      {/* {<CounterTwo />} */}
+      <CounterThree />
     </div>
   );
 }
