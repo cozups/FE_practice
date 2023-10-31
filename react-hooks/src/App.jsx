3347f70e9358f3e52eb8f5db0bf1e822f6975c6b
Kim Miso <kimms9725@naver.com>
2023-10-31 17:59:26 +0900

    custom Hooks

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -1,13 +1,9 @@
-import ClassTimer from './components/useRef/ClassTimer';
-import FocusInput from './components/useRef/FocusInput';
-import HookTimer from './components/useRef/HookTimer';
+import UserForm from './components/customHooks/UserForm';
 
 function App() {
   return (
     <div>
-      <FocusInput />
-      <ClassTimer />
-      <HookTimer />
+      <UserForm />
     </div>
   );
 }
