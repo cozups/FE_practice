d8adf9677d87f54a2f35f4074f65d30daf883cfb
Kim Miso <kimms9725@naver.com>
2023-11-20 19:47:54 +0900

    generic props

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,10 +1,13 @@
-import { Private } from './components/auth/Private';
-import { Profile } from './components/auth/Profile';
+import List from './components/generics/List';
 
 function App() {
   return (
     <div className="App">
-      <Private isLoggedIn={true} component={Profile} />
+      <List
+        items={['Batman', 'Superman', 'Wonder Woman']}
+        onClick={(item) => console.log(item)}
+      />
+      <List items={[1, 2, 3]} onClick={(item) => console.log(item)} />
     </div>
   );
 }
