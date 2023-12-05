2a5e53719f2d4a0f1dc7286f44d8078640e69953
Kim Miso <kimms9725@naver.com>
2023-12-05 15:28:19 +0900

    mapDispatchToProps

--- react-redux/src/components/ItemContainer.jsx
+++ react-redux/src/components/ItemContainer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { buyCake, buyIceCream } from '../redux';
 
 function ItemContainer(props) {
   return (
     <div>
       <h2>Item - {props.item} </h2>
+      <button onClick={props.buyItem}>Buy Item</button>
     </div>
   );
 }
@@ -19,4 +21,15 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default connect(mapStateToProps)(ItemContainer);
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchFunction = ownProps.cake
+    ? () => dispatch(buyCake())
+    : () => dispatch(buyIceCream());
+
+  return {
+    buyItem: dispatchFunction,
+  };
+};
+
+// mapDispatchToProps만 쓰고 싶을 땐 첫 번째 파라미터를 null로 전달
+export default connect(mapStateToProps, mapDispatchToProps)(ItemContainer);
