0ae21e5a6fac67fef022be7099bad8a71323ce8e
Kim Miso <kimms9725@naver.com>
2023-12-06 01:36:02 +0900

    async actions

--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -2,17 +2,19 @@ const store = require('./app/store');
 const cakeActions = require('./features/cake/cakeSlice').cakeActions;
 const iceCreamActions =
   require('./features/iceCream/iceCreamSlice').iceCreamActions;
+const fetchUsers = require('./features/user/userSlice').fetchUsers;
 
 console.log('Initial State ', store.getState());
 const unsubscribe = store.subscribe(() => {});
+store.dispatch(fetchUsers());
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3));
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.ordered());
-store.dispatch(iceCreamActions.restocked(3));
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.ordered());
+// store.dispatch(iceCreamActions.restocked(3));
 
-unsubscribe();
+// unsubscribe();
