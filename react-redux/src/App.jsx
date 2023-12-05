c8a6d23f71f736d191e0410f7fdc6b56a2fd963b
Kim Miso <kimms9725@naver.com>
2023-12-05 16:07:38 +0900

    async actions with redux-thunk

--- react-redux/src/App.jsx
+++ react-redux/src/App.jsx
@@ -5,17 +5,19 @@ import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
 import NewCakeContainer from './components/NewCakeContainer';
 import ItemContainer from './components/ItemContainer';
+import UserContainer from './components/UserContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <ItemContainer cake />
+        {/* <ItemContainer cake />
         <ItemContainer />
         <HooksCakeContainer />
         <CakeContainer />
         <NewCakeContainer />
-        <IceCreamContainer />
+        <IceCreamContainer /> */}
+        <UserContainer />
       </div>
     </Provider>
   );
