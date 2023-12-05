82f69d3143ac8e0fc99649cc3b1dcd8659e20681
Kim Miso <kimms9725@naver.com>
2023-12-06 00:32:37 +0900

    create iceCreamSlice

--- rtk-demo/index.js
+++ rtk-demo/index.js
@@ -1,14 +1,19 @@
 const store = require('./app/store');
 const cakeActions = require('./features/cake/cakeSlice').cakeActions;
+const iceCreamActions =
+  require('./features/iceCream/iceCreamSlice').iceCreamActions;
 
 console.log('Initial State ', store.getState());
 const unsubscribe = store.subscribe(() => {
   console.log('Updated State ', store.getState());
 });
-
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(3));
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.ordered());
+store.dispatch(iceCreamActions.restocked(3));
 
 unsubscribe();
