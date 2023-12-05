daa056cbdeeef5848a659005be21b1e02d2a3f26
Kim Miso <kimms9725@naver.com>
2023-12-05 15:10:28 +0900

    action payload

--- react-redux/src/App.jsx
+++ react-redux/src/App.jsx
@@ -3,6 +3,7 @@ import store from './redux/store';
 import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContainer from './components/IceCreamContainer';
+import NewCakeContainer from './components/NewCakeContainer';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="App">
         <HooksCakeContainer />
         <CakeContainer />
+        <NewCakeContainer />
         <IceCreamContainer />
       </div>
     </Provider>
