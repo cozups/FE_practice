495abc4ff04ce0b78056186f452b71faa64f94e9
Kim Miso <kimms9725@naver.com>
2023-12-06 13:35:28 +0900

    apply Redux toolkit

---
+++ redux-toolkit/src/features/iceCream/iceCreamSlice.js
@@ -0,0 +1,27 @@
+import { ordered as cakeOrdered } from '../cake/cakeSlice';
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  numOfIceCreams: 20,
+};
+
+const iceCreamSlice = createSlice({
+  name: 'iceCream',
+  initialState,
+  reducers: {
+    ordered: (state) => {
+      state.numOfIceCreams--;
+    },
+    restocked: (state, action) => {
+      state.numOfIceCreams += action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(cakeOrdered, (state) => {
+      state.numOfIceCreams--;
+    });
+  },
+});
+
+export default iceCreamSlice.reducer;
+export const { ordered, restocked } = iceCreamSlice.actions;
