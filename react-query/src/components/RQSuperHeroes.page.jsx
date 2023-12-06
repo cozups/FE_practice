99354b370105620b8b4cb780504351fb3193837c
Kim Miso <kimms9725@naver.com>
2023-12-06 19:30:21 +0900

    basic query

--- react-query/src/components/RQSuperHeroes.page.jsx
+++ react-query/src/components/RQSuperHeroes.page.jsx
@@ -1,5 +1,22 @@
+import axios from 'axios';
 import React from 'react';
+import { useQuery } from 'react-query';
 
 export const RQSuperHeroesPage = () => {
-  return <div>RQSuperHeroes page</div>;
+  const { isLoading, data } = useQuery('super-heroes', () => {
+    return axios.get('http://localhost:4000/superheroes');
+  });
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <>
+      <h2>Super Heroes</h2>
+      {data?.data.map((hero) => (
+        <div key={hero.id}>{hero.name}</div>
+      ))}
+    </>
+  );
 };
