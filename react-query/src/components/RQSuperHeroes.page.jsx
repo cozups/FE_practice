7ce1f5aa8ec9fde179f64ea1b747f3981b82a981
Kim Miso <kimms9725@naver.com>
2023-12-08 23:02:36 +0900

    useQuery on click

--- react-query/src/components/RQSuperHeroes.page.jsx
+++ react-query/src/components/RQSuperHeroes.page.jsx
@@ -3,17 +3,17 @@ import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error, isFetching } = useQuery({
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery({
     queryKey: ['super-heroes'],
     queryFn: () => {
       return axios.get('http://localhost:4000/superheroes');
     },
-    staleTime: 30000,
+    enabled: false,
   });
 
   console.log({ isLoading, isFetching });
 
-  if (isLoading) {
+  if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
   }
 
@@ -24,6 +24,7 @@ export const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>Super Heroes</h2>
+      <button onClick={refetch}>Fetch Heroes</button>
       {data?.data.map((hero) => (
         <div key={hero.id}>{hero.name}</div>
       ))}
