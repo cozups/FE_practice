4a55fe0a7bc92b3f9873865e8f99acdd9175cc55
Kim Miso <kimms9725@naver.com>
2023-12-18 20:54:45 +0900

    optimistic update

--- react-query/src/hooks/useSuperHeroesData.js
+++ react-query/src/hooks/useSuperHeroesData.js
@@ -18,14 +18,36 @@ export const useAddSuperHero = () => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: addSuperHero,
-    onSuccess: (data) => {
-      // queryClient.invalidateQueries(['super-heroes']);
+    // onSuccess: (data) => {
+    //   // queryClient.invalidateQueries(['super-heroes']);
+    //   queryClient.setQueryData(['super-heroes'], (oldQueryData) => {
+    //     return {
+    //       ...oldQueryData,
+    //       data: [...oldQueryData.data, data.data],
+    //     };
+    //   });
+    // },
+    onMutate: (newHero) => {
+      queryClient.cancelQueries(['super-heroes']);
+      const previousHeroData = queryClient.getQueryData(['super-heroes']);
       queryClient.setQueryData(['super-heroes'], (oldQueryData) => {
         return {
           ...oldQueryData,
-          data: [...oldQueryData.data, data.data],
+          data: [
+            ...oldQueryData.data,
+            { id: oldQueryData?.data?.length + 1, ...newHero },
+          ],
         };
       });
+      return {
+        previousHeroData,
+      };
+    },
+    onError: (_error, _hero, context) => {
+      queryClient.setQueryData(['super-heroes'], context.previousHeroData);
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(['super-heroes']);
     },
   });
 };
