7460e72fe2e70e857f20eedcde0792773286ad35
Kim Miso <kimms9725@naver.com>
2024-01-11 19:56:38 +0900

    write test and TDD

---
+++ react-testing/react-testing-project/src/components/greet/Greet.test.tsx
@@ -0,0 +1,19 @@
+/**
+ * Greet should render the text hello and if a name is passed into the component
+ * It should render hello followed by the name
+ */
+
+import { render, screen } from '@testing-library/react';
+import { Greet } from './Greet';
+
+test('Greet renders correctly', () => {
+  render(<Greet />);
+  const textElement = screen.getByText('Hello');
+  expect(textElement).toBeInTheDocument();
+});
+
+test('Greet renders with a name', () => {
+  render(<Greet name="cozups" />);
+  const textElement = screen.getByText('Hello cozups');
+  expect(textElement).toBeInTheDocument();
+});
