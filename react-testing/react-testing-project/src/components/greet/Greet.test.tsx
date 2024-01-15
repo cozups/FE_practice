45281c65c5ee2959fe21c13912030dd8b1190bde
Kim Miso <kimms9725@naver.com>
2024-01-15 14:38:52 +0900

    code coverage

--- react-testing/react-testing-project/src/components/greet/Greet.test.tsx
+++ react-testing/react-testing-project/src/components/greet/Greet.test.tsx
@@ -9,15 +9,7 @@ import { Greet } from './Greet';
 describe('Greet', () => {
   test('renders correctly', () => {
     render(<Greet />);
-    const textElement = screen.getByText('Hello');
-    expect(textElement).toBeInTheDocument();
-  });
-});
-
-describe('Nested', () => {
-  test('renders a name', () => {
-    render(<Greet name="cozups" />);
-    const textElement = screen.getByText('Hello cozups');
+    const textElement = screen.getByText(/Hello/);
     expect(textElement).toBeInTheDocument();
   });
 });
