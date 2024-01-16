aeb0f853056cfbe7caa7e4e0a5f1a5da45618172
Kim Miso <kimms9725@naver.com>
2024-01-16 17:19:56 +0900

    findBy

--- react-testing/react-testing-project/src/components/skills/Skills.test.tsx
+++ react-testing/react-testing-project/src/components/skills/Skills.test.tsx
@@ -31,4 +31,18 @@ describe('Skills', () => {
     });
     expect(startLearningButton).not.toBeInTheDocument();
   });
+
+  test('Start Learning button is eventually displayed', async () => {
+    render(<Skills skills={skills} />);
+    const startLearningButton = await screen.findByRole(
+      'button',
+      {
+        name: 'Start Learning',
+      },
+      {
+        timeout: 2000,
+      }
+    );
+    expect(startLearningButton).toBeInTheDocument();
+  });
 });
