3e9e7cd93a198c77f15e358b774973e25075dd6d
Kim Miso <kimms9725@naver.com>
2024-01-23 14:34:54 +0900

    debugging

--- react-testing/react-testing-project/src/components/skills/Skills.test.tsx
+++ react-testing/react-testing-project/src/components/skills/Skills.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, logRoles } from '@testing-library/react';
 import { Skills } from './Skills';
 
 describe('Skills', () => {
@@ -33,7 +33,9 @@ describe('Skills', () => {
   });
 
   test('Start Learning button is eventually displayed', async () => {
-    render(<Skills skills={skills} />);
+    const view = render(<Skills skills={skills} />);
+    logRoles(view.container);
+    // screen.debug();
     const startLearningButton = await screen.findByRole(
       'button',
       {
@@ -43,6 +45,7 @@ describe('Skills', () => {
         timeout: 2000,
       }
     );
+    // screen.debug();
     expect(startLearningButton).toBeInTheDocument();
   });
 });
