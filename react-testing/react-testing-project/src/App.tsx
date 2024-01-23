5df426b131d35d5abde011af4fc75a33664513e6
Kim Miso <kimms9725@naver.com>
2024-01-23 15:11:10 +0900

    mouse interaction

--- react-testing/react-testing-project/src/App.tsx
+++ react-testing/react-testing-project/src/App.tsx
@@ -1,9 +1,9 @@
-import { Application } from './components/application/Application';
+import { Counter } from './components/counter/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Application />
+      <Counter />
     </div>
   );
 }
