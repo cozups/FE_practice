f2a29a54f33f324e2261c26aeaafe21271ac3bf2
Kim Miso <kimms9725@naver.com>
2024-03-25 09:08:25 +0900

    metadata

--- Nextjs-tutorial/nextjs-codevolution/src/app/products/[productId]/page.tsx
+++ Nextjs-tutorial/nextjs-codevolution/src/app/products/[productId]/page.tsx
@@ -1,10 +1,18 @@
-export default function ProductDetailPage({
-  params,
-}: {
+import { Metadata } from 'next';
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return {
+    title: `Product ${params.productId}`,
+  };
+};
+
+type Props = {
   params: {
     productId: string;
   };
-}) {
+};
+
+export default function ProductDetailPage({ params }: Props) {
   return (
     <div>
       <h1>Product Detail page {params.productId}</h1>
