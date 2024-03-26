888a6df24708b8083d5ca14f5366d2d499668a6c
Kim Miso <kimms9725@naver.com>
2024-03-26 20:14:58 +0900

    unmatched route

--- Nextjs-tutorial/nextjs-codevolution/src/app/complex-dashboard/@notifications/page.tsx
+++ Nextjs-tutorial/nextjs-codevolution/src/app/complex-dashboard/@notifications/page.tsx
@@ -1,5 +1,11 @@
 import Card from '@/app/components/card';
+import Link from 'next/link';
 
 export default function NotificationPage() {
-  return <Card>Notification</Card>;
+  return (
+    <Card>
+      <div>Notifications</div>
+      <Link href="/complex-dashboard/archived">Archived</Link>
+    </Card>
+  );
 }
---
+++ Nextjs-tutorial/nextjs-codevolution/src/app/complex-dashboard/@users/default.tsx
@@ -0,0 +1,5 @@
+import Card from '@/app/components/card';
+
+export default function DefaultUserPage() {
+  return <Card>User</Card>;
+}
