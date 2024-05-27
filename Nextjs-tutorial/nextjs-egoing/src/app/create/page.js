5e0e5815d6bc8e0ccd86aba3fae140b9b13b9eb4
Kim Miso <kimms9725@naver.com>
2024-05-27 17:11:04 +0900

    CRUD - create data

--- Nextjs-tutorial/nextjs-egoing/src/app/create/page.js
+++ Nextjs-tutorial/nextjs-egoing/src/app/create/page.js
@@ -1,3 +1,43 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import { useRef } from 'react';
 export default function Create() {
-  return <>Create!</>;
+  const router = useRouter();
+  const titleRef = useRef();
+  const bodyRef = useRef();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const title = titleRef.current.value;
+    const body = bodyRef.current.value;
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title, body }),
+    };
+    fetch(`http://localhost:9999/topics`, options)
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        const lastId = result.id;
+        router.push(`/read/${lastId}`);
+        router.refresh();
+      });
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <p>
+        <input type="text" name="title" placeholder="title" ref={titleRef} />
+      </p>
+      <p>
+        <textarea name="body" placeholder="body" ref={bodyRef}></textarea>
+      </p>
+      <p>
+        <input type="submit" value="create" />
+      </p>
+    </form>
+  );
 }
