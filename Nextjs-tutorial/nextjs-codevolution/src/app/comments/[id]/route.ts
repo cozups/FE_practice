2964fafec87041dd02392813bfeefb62a63893c4
Kim Miso <kimms9725@naver.com>
2024-06-14 00:26:33 +0900

    PATCH, DELETE routes

--- Nextjs-tutorial/nextjs-codevolution/src/app/comments/[id]/route.ts
+++ Nextjs-tutorial/nextjs-codevolution/src/app/comments/[id]/route.ts
@@ -10,3 +10,31 @@ export async function GET(
 
   return Response.json(comment);
 }
+
+export async function PATCH(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { text } = await request.json();
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+
+  comments[index].text = text;
+
+  return Response.json(comments[index]);
+}
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const index = comments.findIndex(
+    (comment) => comment.id === parseInt(params.id)
+  );
+
+  const deletedComment = comments[index];
+  comments.splice(index, 1);
+
+  return Response.json(deletedComment);
+}
