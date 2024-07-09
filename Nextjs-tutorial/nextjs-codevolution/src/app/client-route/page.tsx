580bf13cbb70b2df4c93cfa2b04b54e1f1c1ea9d
Kim Miso <kimms9725@naver.com>
2024-07-09 15:10:00 +0900

    client-only code

--- Nextjs-tutorial/nextjs-codevolution/src/app/client-route/page.tsx
+++ Nextjs-tutorial/nextjs-codevolution/src/app/client-route/page.tsx
@@ -1,19 +1,18 @@
 'use client';
 
 import { useTheme } from '@/app/components/themeProvider';
+import { clientSideFunction } from '@/app/utils/client-utils';
 
 export default function ClientRoutePage() {
   const theme = useTheme();
-  const settings = {
-    dots: true,
-  };
+  const result = clientSideFunction();
   return (
     <h1
       style={{
         color: theme.colors.secondary,
       }}
     >
-      Client Route Page
+      Client Route {result}
     </h1>
   );
 }
