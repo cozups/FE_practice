fc5097e05c7effaf5c9cc55ae7289baaf1aa83c2
Kim Miso <kimms9725@naver.com>
2024-07-09 14:08:11 +0900

    streaming

---
+++ Nextjs-tutorial/nextjs-codevolution/src/app/product-detail/page.tsx
@@ -0,0 +1,18 @@
+import { Suspense } from 'react';
+
+import { Product } from '@/app/components/product';
+import { Reviews } from '@/app/components/reviews';
+
+export default function ProductDetailPage() {
+  return (
+    <div>
+      <h1>Product detail page</h1>
+      <Suspense fallback={<p>Loading product details...</p>}>
+        <Product />
+      </Suspense>
+      <Suspense fallback={<p>Loading reviews...</p>}>
+        <Reviews />
+      </Suspense>
+    </div>
+  );
+}
