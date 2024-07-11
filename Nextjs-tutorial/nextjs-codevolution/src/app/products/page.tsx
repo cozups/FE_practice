fb10cbcc42f92fd625f6b2ac8fe0da14d3da801c
Kim Miso <kimms9725@naver.com>
2024-07-11 19:08:41 +0900

    caching data & opting out caching

--- Nextjs-tutorial/nextjs-codevolution/src/app/products/page.tsx
+++ Nextjs-tutorial/nextjs-codevolution/src/app/products/page.tsx
@@ -1,19 +1,36 @@
-import Link from 'next/link';
+import { cookies } from 'next/headers';
+// export const fetchCache = 'default-cache';
 
-export default function ProductPage() {
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+};
+
+export default async function ProductsPage() {
+  const response = await fetch('http://localhost:3001/products');
+  const products = await response.json();
+
+  const cookieStore = cookies();
+  const theme = cookieStore.get('theme');
+
+  const detailsResponse = await fetch('http://localhost:3001/products/1');
+  const details = await detailsResponse.json();
+
   return (
-    <div>
-      <Link href="/">Home</Link>
-      <h1>Product List</h1>
-      <h2>
-        <Link href="/products/1">Product 1</Link>
-      </h2>
-      <h2>
-        <Link href="/products/2">Product 2</Link>
-      </h2>
-      <h2>
-        <Link href="/products/3">Product 3</Link>
-      </h2>
-    </div>
+    <ul className="space-y-4 p-4">
+      {products.map((product: Product) => (
+        <li
+          key={product.id}
+          className="p-4 bg-white shadow-md rounded-lg text-gray-700"
+        >
+          <h2 className="text-xl font-semibold">{product.title}</h2>
+          <p>{product.description}</p>
+          <p className="text-lg font-medium">${product.price}</p>
+          <p>{details.price}</p>
+        </li>
+      ))}
+    </ul>
   );
 }
