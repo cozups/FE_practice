1785fcf5fb1e288762088c36e98cf34eb79219b7
Kim Miso <kimms9725@naver.com>
2024-10-28 20:33:16 +0900

    feat: style main-header and optimize image

--- nextjs-udemy/05-onwards-foodies-starting-project/components/main-header.js
+++ nextjs-udemy/05-onwards-foodies-starting-project/components/main-header.js
@@ -1,16 +1,18 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 import logoImg from '@/assets/logo.png';
+import styles from './main-header.module.css';
 
 export default function MainHeader() {
   return (
-    <header>
-      <Link href="/">
-        <img src={logoImg.src} alt="A plate with food on it" />
+    <header className={styles.header}>
+      <Link href="/" className={styles.logo}>
+        <Image src={logoImg} alt="A plate with food on it" priority />
         NextLevel Food
       </Link>
 
-      <nav>
+      <nav className={styles.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>
