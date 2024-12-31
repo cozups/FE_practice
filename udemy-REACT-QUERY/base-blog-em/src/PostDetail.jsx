ba6e48212a44a347e434e89f497f0d76ac5dd289
Miso Kim <kimms9725@naver.com>
2024-12-31 17:52:11 +0900

    new project: udemy-react-query

---
+++ udemy-REACT-QUERY/base-blog-em/src/PostDetail.jsx
@@ -0,0 +1,36 @@
+import { useQuery } from '@tanstack/react-query';
+import { fetchComments } from './api';
+import './PostDetail.css';
+
+export function PostDetail({ post }) {
+  // replace with useQuery
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['comments', post.id],
+    queryFn: () => fetchComments(post.id),
+  });
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+  if (isError) {
+    return (
+      <div>
+        <h3>Oops, something went wrong</h3>
+        <p>{error.toString()}</p>
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <h3 style={{ color: 'blue' }}>{post.title}</h3>
+      <button>Delete</button> <button>Update title</button>
+      <p>{post.body}</p>
+      <h4>Comments</h4>
+      {data.map((comment) => (
+        <li key={comment.id}>
+          {comment.email}: {comment.body}
+        </li>
+      ))}
+    </>
+  );
+}
