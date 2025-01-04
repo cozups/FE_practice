f9926365ba4620a32e11ef56349ee185d76f8ca2
Miso Kim <kimms9725@naver.com>
2025-01-04 23:43:30 +0900

    Section #3 - infinite query

--- udemy-REACT-QUERY/base-infinite-swapi/src/App.jsx
+++ udemy-REACT-QUERY/base-infinite-swapi/src/App.jsx
@@ -1,14 +1,21 @@
-import "./App.css";
-import { InfinitePeople } from "./people/InfinitePeople";
-import { InfiniteSpecies } from "./species/InfiniteSpecies";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './App.css';
+import { InfinitePeople } from './people/InfinitePeople';
+import { InfiniteSpecies } from './species/InfiniteSpecies';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <div className="App">
-      <h1>Infinite SWAPI</h1>
-      <InfinitePeople />
-      {/* <InfiniteSpecies /> */}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <div className="App">
+        <h1>Infinite SWAPI</h1>
+        <InfinitePeople />
+        {/* <InfiniteSpecies /> */}
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- udemy-REACT-QUERY/base-infinite-swapi/src/people/InfinitePeople.jsx
+++ udemy-REACT-QUERY/base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,7 +1,8 @@
-import InfiniteScroll from "react-infinite-scroller";
-import { Person } from "./Person";
+import InfiniteScroll from 'react-infinite-scroller';
+import { Person } from './Person';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
-const initialUrl = "https://swapi.dev/api/people/";
+const initialUrl = 'https://swapi.py4e.com/api/people/';
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
@@ -9,5 +10,51 @@ const fetchUrl = async (url) => {
 
 export function InfinitePeople() {
   // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isLoading,
+    isError,
+    error,
+  } = useInfiniteQuery({
+    queryKey: ['sw-people'],
+    queryFn: ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    getNextPageParam: (lastPage) => {
+      return lastPage.next || undefined;
+    },
+  });
+
+  if (isLoading) {
+    return <div className="loading">loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error: {error.toString()}</div>;
+  }
+
+  return (
+    <InfiniteScroll
+      hasMore={hasNextPage}
+      loadMore={() => {
+        if (!isFetching) {
+          fetchNextPage();
+        }
+      }}
+    >
+      {data.pages.map((pageData) => {
+        return pageData.results.map((person) => {
+          return (
+            <Person
+              key={person.name}
+              name={person.name}
+              hairColor={person.hair_color}
+              eyeColor={person.eye_color}
+            />
+          );
+        });
+      })}
+    </InfiniteScroll>
+  );
 }
