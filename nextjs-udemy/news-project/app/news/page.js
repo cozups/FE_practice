88e5cad3a40b80d8930f06bbb33d78b2a5aaa286
Miso Kim <kimms9725@naver.com>
2025-02-20 22:00:08 +0900

    learn parallel route

--- nextjs-udemy/news-project/app/news/page.js
+++ nextjs-udemy/news-project/app/news/page.js
@@ -1,20 +1,9 @@
-import Link from 'next/link';
-
 import { DUMMY_NEWS } from '@/dummy-news';
 
 export default function NewsPage() {
   return (
     <div>
-      <ul className="news-list">
-        {DUMMY_NEWS.map((news) => (
-          <li key={news.id}>
-            <Link href={`/news/${news.slug}`}>
-              <img src={`/images/news/${news.image}`} alt={news.title} />
-              <span>{news.title}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <NewsPage news={DUMMY_NEWS} />
     </div>
   );
 }
