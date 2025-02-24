1eb9ceedebc691a2d6bdad7e21f665175aaba8a9
Miso Kim <kimms9725@naver.com>
2025-02-24 15:15:50 +0900

    fix error

--- nextjs-udemy/news-project/app/news/page.js
+++ nextjs-udemy/news-project/app/news/page.js
@@ -1,9 +1,10 @@
+import NewsList from '@/components/news-list';
 import { DUMMY_NEWS } from '@/dummy-news';
 
 export default function NewsPage() {
   return (
     <div>
-      <NewsPage news={DUMMY_NEWS} />
+      <NewsList news={DUMMY_NEWS} />
     </div>
   );
 }
