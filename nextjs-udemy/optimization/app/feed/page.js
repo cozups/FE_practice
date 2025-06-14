22468acc10b861e3e32fcede8184ab5de71a49ac
Miso Kim <kimms9725@naver.com>
2025-06-14 17:10:47 +0900

    generate metadata

--- nextjs-udemy/optimization/app/feed/page.js
+++ nextjs-udemy/optimization/app/feed/page.js
@@ -1,6 +1,21 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// export const metadata = {
+//   title: 'All Posts',
+//   description: 'Browse all our posts.',
+// };
+
+export async function generateMetadata() {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+
+  return {
+    title: `All ${numberOfPosts} posts.`,
+    description: 'Browse all our posts.',
+  };
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
